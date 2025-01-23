cb8bdf6538ab72e1971e0e1d76537f30d3b9e582
Tiago <costarego.tiago@gmail.com>
2025-01-23 12:24:56 +0100

    mis a jour de evalInst

--- calcBaseV2.py
+++ calcBaseV2.py
@@ -6,13 +6,13 @@ reserved = {
     'print': 'PRINT',
     'if':'IF',
     'while':'WHILE',
-    'for':'FOR'
-
+    'for':'FOR',
+    'return':'RETURN',
 }
 
 tokens = ['NUMBER', 'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'LPAREN',
           'RPAREN', 'OR', 'AND', 'SEMI', 'EGAL', 'NAME', 'INF', 'SUP',
-          'EGALEGAL', 'INFEG', 'LBRACE', 'RBRACE'] + list(reserved.values())
+          'EGALEGAL', 'INFEG', 'LBRACE', 'RBRACE', 'COMMA'] + list(reserved.values())
 
 t_PLUS = r'\+'
 t_MINUS = r'-'
@@ -23,43 +23,63 @@ t_RPAREN = r'\)'
 t_OR = r'\|'
 t_AND = r'\&'
 t_SEMI = r';'
-t_EGAL = r'\='
-# t_NAME = r'[a-zA-Z_][a-zA-Z_0-9]*'
+t_EGAL = r'='
 t_INF = r'\<'
 t_SUP = r'>'
-t_INFEG = r'\<\='
-t_EGALEGAL = r'\=\='
+t_INFEG = r'\<='
+t_EGALEGAL = r'=='
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
+t_COMMA = r','
 
+global_scope ={}
+local_scope =[{}]
+
+def get_variable(name):
+    for scope in reversed(local_scope):
+        if name in scope:
+            return scope[name]
+    if name in global_scope:
+        return global_scope[name]
+    raise NameError(f"Variable '{name}' non trouvée")
+
+def set_variable(name, value):
+    for scope in reversed(local_scope):
+        if name in scope:
+            scope[name] = value
+            return
+    global_scope[name] = value
+
+def enter_scope():
+    local_scope.append({})
+
+def exit_scope():
+    if len(local_scope) > 1:
+        local_scope.pop()
+    else:
+        raise Exception("Peut pas partir")
 
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'NAME')  # Check for reserved words
+    t.type = reserved.get(t.value, 'NAME')  
     return t
-
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-
 t_ignore = " \t"
-
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
-
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-
 import ply.lex as lex
-
 lex.lex()
 
 names = {}
@@ -72,25 +92,47 @@ precedence = (
 )
 
 def evalInst(p):
-    if type(p) == tuple:
-
+    if isinstance(p, tuple):
         def evalExpr(t):
-            if type(t) == int: return t
-            if type(t) == str: return names[t]
-            if t[0] == '+': return eval(t[1]) + eval(t[2])
-            if t[0] == '*': return eval(t[1]) * eval(t[2])
-            if t[0] == '/': return eval(t[1]) / eval(t[2])
-            if t[0] == '-': return eval(t[1]) - eval(t[2])
-
-        if p[0] == 'print': print(evalExpr(p[1]))
-
+            if isinstance(t, int): 
+                return t
+            if isinstance(t, str): 
+                return get_variable(t)
+            if t[0] == '+': 
+                return eval(t[1]) + eval(t[2])
+            if t[0] == '-': 
+                return eval(t[1]) - eval(t[2])
+            if t[0] == '*': 
+                return eval(t[1]) * eval(t[2])
+            if t[0] == '/': 
+                return eval(t[1]) // eval(t[2])
+            if t[0] == '<': 
+                return eval(t[1]) < eval(t[2])
+            if t[0] == '>': 
+                return eval(t[1]) > eval(t[2])
+        
+        if p[0] == 'print':
+            print(evalExpr(p[1]))
+        elif p[0] == 'assign':
+            set_variable(p[1], evalExpr(p[2]))
+        elif p[0] == 'if':
+            condition = evalExpr(p[1])
+            if condition:
+                evalInst(p[2])
+        elif p[0] == 'while':
+            while evalExpr(p[1]):
+                evalInst(p[2])
+        elif p[0] == 'for':
+            evalInst(p[1])
+            while evalExpr(p[2]):  
+                evalInst(p[4])
+                evalInst(p[3])  
 
 def p_start(p):
     'start : bloc'
     print(p[1])
     printTreeGraph(p[1])
     evalInst(p[1])
-
 
 def p_bloc(p):
     '''bloc : bloc statement SEMI
@@ -100,21 +142,17 @@ def p_bloc(p):
     else:
         p[0] = ('bloc', p[1], p[3])
 
-
 def p_statement_expr(p):
     'statement : PRINT LPAREN expression RPAREN'
-    # print(p[3])
     p[0] = ('print', p[3])
-
 
 def p_statement_assign(p):
     'statement : NAME EGAL expression'
-    # names[p[1]]=p[3]
     p[0] = ('assign', p[1], p[3])
 
 def p_statement_if(p):
     'statement : IF LPAREN expression RPAREN statement'
-    p[0] = ('if', p[3],p[5])
+    p[0] = ('if', p[3], p[5])
 
 def p_statement_for(p):
     'statement : FOR LPAREN statement SEMI expression SEMI statement RPAREN statement'
@@ -122,7 +160,7 @@ def p_statement_for(p):
 
 def p_statement_while(p):
     'statement : WHILE LPAREN expression RPAREN statement '
-    p[0] = ('while',p[3], p[5])
+    p[0] = ('while', p[3], p[5])
 
 def p_param_list(p):
     '''param_list : 
@@ -137,16 +175,17 @@ def p_param_list(p):
 
 def p_statement_return(p):
     'statement : RETURN expression'
-    if len(p) == 1:
-        p[0] = [] 
-    else: 
-        p[0] = ('return', p[2])
+    p[0] = ('return', p[2])  
+
+def p_statement_return_empty(p):
+    'statement : RETURN'
+    p[0] = ('return', None) 
 
 def p_statement_fonction(p):
     'statement : NAME LPAREN param_list RPAREN LBRACE bloc RBRACE '
+    enter_scope()  
     p[0] = (p[3], p[5])
-
-
+    exit_scope()  
 
 def p_expression_binop_inf(p):
     '''expression : expression INF expression
@@ -161,16 +200,13 @@ def p_expression_binop_inf(p):
     | expression SUP expression'''
     p[0] = (p[2], p[1], p[3])
 
-
 def p_expression_group(p):
     'expression : LPAREN expression RPAREN'
     p[0] = p[2]
 
-
 def p_expression_number(p):
     'expression : NUMBER'
     p[0] = p[1]
-
 
 def p_expression_name(p):
     'expression : NAME'
@@ -179,7 +215,6 @@ def p_expression_name(p):
 def p_error(p):    print("Syntax error in input!")
 
 import ply.yacc as yacc
-
 yacc.yacc()
 s = 'print(1+2);'
 yacc.parse(s)
